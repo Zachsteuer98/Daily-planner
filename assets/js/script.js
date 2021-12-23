84d710442530285043af02044a7debb1e26ccd3f
Zachsteuer98 <Zach.Steuer98@gmail.com>
2021-12-22 21:08:50 -0500

    working on color code hours in task-content

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var Today = (moment().format("MMMM D, YYYY"))
+    $("#currentDay").text(Today);
+
+//color code hours Credit too mlportu git https://github.com/mlportu/workday-scheduler/blob/master/assets/script.js
+var hourAudit=function(){
+    var currentHour= moment().hour() 
+
+    for(var i = 8; i < 19; i++); {
+        var taskArea = $("#task-content-" + i)
+        if(currentHour>i){
+            $(taskArea).addClass("past");
+        } else if (currentHour === i){
+            $(taskArea).addClass("present");
+        }else {
+            $(taskArea).addClass("future");
+        }
+    } 
+}
+
+setInterval(function(){
+    hourAudit();},1000*60*60);
+
+    hourAudit()
